f54d813ec8157132195d69e986220b3f39d518fd
Guilherme Ribeiro <guilhermevendaseprodutos@gmail.com>
2024-02-20 09:56:51 -0300

    Positivo ou negativo

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,27 +9,17 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		int cod1, cod2, quantity1, quantity2;
-		double price1, price2, totalValue;
+		int number;
 		
-		System.out.println(" Codigo do primeiro produtos");
-		cod1 = sc.nextInt();
-		System.out.println("quantity product: ");
-		quantity1 = sc.nextInt();
-		System.out.println("price one product: ");
-		price1 = sc.nextDouble();
+		System.out.println(" Digite um numero: ");
+		number = sc.nextInt();
 		
-		
-		System.out.println(" Codigo do segundo produtos");
-		cod2 = sc.nextInt();
-		System.out.println("quantity product: ");
-		quantity2 = sc.nextInt();
-		System.out.println("price two product: ");
-		price2 = sc.nextDouble();
-		
-		totalValue = (quantity1 * price1) + (quantity2 * price2);
-		
-		System.out.println("o valor total a pagar e de: " + totalValue);
+		if(number < 0) {
+			System.out.println("ESTE NUMERO E NEGATIVO");
+		}
+		else {
+			System.out.println("ESTE NUMERO E POSITIVO");
+		}
 		
 		
 		
