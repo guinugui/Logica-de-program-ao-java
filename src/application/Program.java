41da6f8f965b210e9e9b5b0efefe13144ceae5b7
Guilherme Ribeiro <guilhermevendaseprodutos@gmail.com>
2024-02-22 10:20:49 -0300

    plano cartesiado x, y

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,16 +9,36 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		int senha;
+		int x, y;
 		
-		System.out.println("Digite a senha correta");
-		senha = sc.nextInt();
-		while(senha != 2002) {
-			System.out.println("senha incorreta");
-			senha = sc.nextInt();
+		System.out.println("Digite x: ");
+		x = sc.nextInt();
+		System.out.println("Digite x: ");
+		y = sc.nextInt();
+		
+		
+		
+		while(x != 0 && y != 0) {
+			if(x > 0 && y > 0) {
+				System.out.println("primeiro");
+			}
+			else if(x < 0 && y > 0) {
+				System.out.println("segundo");
+			}
+			else if(x < 0 && y < 0) {
+				System.out.println("terceiro");
+			}
+			else if(x > 0 && y < 0) {
+				System.out.println("quarto");
+			}
+			System.out.println("Digite x: ");
+			x = sc.nextInt();
+			System.out.println("Digite Y: ");
+			y = sc.nextInt();
+		
 		}
 
-		System.out.println("Acesso permitido");
+		System.out.println("Encerrado!");
 		
 
 
